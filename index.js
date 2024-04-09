588d89fa727fea7f98252541fcaaedf998d221ae
Cedric Glou <cedricglou@MacBook-Air-de-Cedric.local>
2024-04-09 20:12:54 +0200

    projet fini

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const color1 = document.querySelector(".color-square1");
 const color2 = document.querySelector(".color-square2");
 const color3 = document.querySelector(".color-square3");
 
+const rangeInput = document.querySelector("#rangeInput");
+const labelRangeInput = document.querySelector("#labelRange");
+console.log(rangeInput.value);
+// color de base
+let color = "black";
+// size de base
+let size = 1;
+
+rangeInput.addEventListener("input", () => {
+  console.log(rangeInput.value);
+  //   actualise en temps reel le range
+  labelRangeInput.innerHTML = rangeInput.value;
+  //   return la nouvelle valeur de range
+  return (size = rangeInput.value);
+});
+
 const reset = document.getElementById("reset");
 console.log(reset);
 
@@ -20,7 +36,6 @@ function getMousePosition(e) {
     y: e.clientY - rect.top,
   };
 }
-let color = "black";
 
 color1.addEventListener("click", (e) => {
   console.log(e);
@@ -46,7 +61,7 @@ function mouseMove(e) {
   ctx.stroke();
 
   ctx.strokeStyle = color;
-  ctx.lineWidth = 10;
+  ctx.lineWidth = size;
 }
 
 canvas.addEventListener("mousedown", (e) => {
